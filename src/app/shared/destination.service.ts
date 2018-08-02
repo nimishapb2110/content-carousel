7fc188192d4fae2e13ba7857c812d86aaef40632
nimishapb <nimishapb2110@gmail.com>
2018-08-02 21:56:01 +0200

    updated service call, modified arrow

--- src/app/shared/destination.service.ts
+++ src/app/shared/destination.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CarouselModel } from '../carousel/carousel.model';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
