02aa82ed70124d075643b3a05d96b041ff0ffdca
nimishapb <nimishapb2110@gmail.com>
2018-08-02 19:35:59 +0200

    included additional features

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DestinationService } from '../shared/destination.service';
 import { CarouselModel } from '../carousel/carousel.model';
-import { Theme } from '../shared/theme.model';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +8,11 @@ import { Theme } from '../shared/theme.model';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  @Input() theme: Theme;
   carouselDataList: Array<CarouselModel>;
   constructor( private destinationService: DestinationService ) { }
 
   ngOnInit() {
     this.carouselDataList = this.destinationService.getDestinationList();
-  }
-  get primary() {
-    return this.theme.primary;
   }
 
 }
