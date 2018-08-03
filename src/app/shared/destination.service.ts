f33288e1912c43de31d6022eecd2014aecd2abf1
nimishapb <nimishapb2110@gmail.com>
2018-08-03 18:22:50 +0200

    comments included

--- src/app/shared/destination.service.ts
+++ src/app/shared/destination.service.ts
@@ -1,19 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CarouselModel } from '../carousel/carousel.model';
 import { HttpClient } from '@angular/common/http';
-import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DestinationService {
-  destinationList: Array<CarouselModel>;
-  private destinationData = new Subject<Array<CarouselModel>>();
   constructor( private http: HttpClient ) { }
-
-  getDestinationUpdatedListener() {
-    return this.destinationData.asObservable();
-  }
 
   getDestinationList() {
     return this.http.get<Array<CarouselModel>>('../../assets/mock/destinations.json');
